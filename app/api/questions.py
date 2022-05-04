03ab009261637ccfdd1a2fc92a89b5db0c3b6dc3
Entsllor <slava90100@yandex.com>
2022-05-04 12:28:53 +0500

    feat: make new response if count of new questions less than count of expected ones

--- app/api/questions.py
+++ app/api/questions.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from httpx import AsyncClient
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import models
@@ -10,11 +11,23 @@ router = APIRouter()
 client = AsyncClient()
 
 
+async def get_not_existence_questions_ids(db, ids: set[int]):
+    existence_ids = (await db.execute(select(models.Question.id).where(models.Question.id.in_(ids)))).all()
+    return ids - set(existence_ids)
+
+
 @router.post('/', response_model=list[QuestionOut])
 async def create_question(questions_create: QuestionsCreate, db: AsyncSession = Depends(get_db)):
-    response = await client.get(f"https://jservice.io/api/random?count={questions_create.questions_num}")
-    questions = [QuestionOut(**question) for question in response.json()]
-    db_questions = [models.Question(**question.dict()) for question in questions]
+    new_questions = []
+    new_ids = set()
+    while len(new_ids) != questions_create.questions_num:
+        response = await client.get(f"https://jservice.io/api/random?count={questions_create.questions_num}")
+        body = response.json()
+        question_ids = {question['id'] for question in body}
+        new_ids |= await get_not_existence_questions_ids(db, question_ids)
+        new_questions.extend([QuestionOut(**question) for question in body if question['id'] in new_ids])
+
+    db_questions = [models.Question(**question.dict()) for question in new_questions]
     db.add_all(db_questions)
     await db.commit()
-    return response.json()
+    return new_questions
